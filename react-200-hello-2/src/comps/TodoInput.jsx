79816dd4dbcdb9d1792d7809f7dd54321b05becc
jinwoo8420 <1223wlsdn@naver.com>
2022-06-30 14:56:10 +0900

    0630

--- react-200-hello-2/src/comps/TodoInput.jsx
+++ react-200-hello-2/src/comps/TodoInput.jsx
@@ -1,9 +1,28 @@
-const TodoInput = () => {
+const TodoInput = (props) => {
+  const { insert_todo } = props;
+
+  /*
+    input box에 키보드로 문자열을 입력하는 중에 Enter를 누르면
+    데이터를 TodoMain의 insert_todo에게 보내서 todoList에 추가 요청
+  */
+
+  const onKeyDown = (e) => {
+    if (e.keyCode === 13) {
+      // insert_todo 호출하며 입력된 todo 전송
+      insert_todo(e.target.value);
+    }
+  };
+
   return (
     <>
+      {/* 
+      input box onKeyDown 핸들러 등록 
+      키보드 입력을 감시
+      */}
       <input
         placeholder="TODO INPUT"
         className="w3-input w3-border w3-hover-light-gray"
+        onKeyDown={onKeyDown}
       />
     </>
   );
--- react-200-hello-2/src/comps/TodoMain.jsx
+++ react-200-hello-2/src/comps/TodoMain.jsx
@@ -1,19 +1,34 @@
 import { useState } from "react";
+import moment from "moment";
 import TodoInput from "./TodoInput";
 import TodoList from "./TodoList";
 
 const TodoMain = () => {
-  const { todoList, setTodoList } = useState([]);
+  const [todoList, setTodoList] = useState([]);
 
-  const insert_todo = () => {
+  const insert_todo = (content) => {
     // TodoInput이 전달해준 todo를 todoList에 추가하는 곳
+    const todo = {
+      t_id: "",
+      t_s_date: moment().format("YYYY[-]MM[-]DD HH:mm:ss"),
+      content: content,
+      t_e_date: "",
+    };
+
+    /*
+        todoList 배열을 모두 복사하고
+        끝에 todo를 추가하여 todoBody 배열 생성
+    */
+    const todoBody = [...todoList, todo];
+    setTodoList(todoBody);
   };
 
   return (
     <div className="w3-container w3-margin">
-      {/* TodoInput => insert_todo */}
+      {/* insert_todo => TodoInput */}
       <TodoInput insert_todo={insert_todo} />
-      <TodoList />
+      {/* todoList 데이터를 todoList 변수에 담고 TodoList로 보내기 */}
+      <TodoList todoList={todoList} />
     </div>
   );
 };
